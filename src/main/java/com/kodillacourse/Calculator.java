963caed4b27500b0a396794f95e995ae138d6429
JoaKam <joannakamienik@wp.pl>
2017-07-29 20:35:45 +0200

    Initial

---
+++ src/main/java/com/kodillacourse/Calculator.java
@@ -0,0 +1,4 @@
+package com.kodillacourse;
+
+class Calculator {
+}
---
+++ src/main/java/com/kodillacourse/ExtraCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.kodillacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtraCalculatorApplication.class, args);
+	}
+}
